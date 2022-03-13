026fc3664981963e20e643eefa4b2866c5ae9924
Sandy McCabe <sandytech13@gmail.com>
2022-03-12 22:29:55 -0600

    pass repo through pages and add error handling.

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,23 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    // console.log(repoName);
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+
+}
 
 
 var getRepoIssues = function (repo) {
@@ -15,18 +33,19 @@ var getRepoIssues = function (repo) {
 
                 // check if api has paginated issues
                 if (response.headers.get("Link")) {
-                   displayWarning(repo);
+                    displayWarning(repo);
                 }
 
             });
         }
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
 };
 
-var displayWarning = function(repo) {
+var displayWarning = function (repo) {
     // add text to warning container
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
@@ -34,10 +53,10 @@ var displayWarning = function(repo) {
     linkEl.textContent = "See More Issues on GitHub.com";
     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
     linkEl.setAttribute("target", "_blank");
-  
+
     // append to warning container
     limitWarningEl.appendChild(linkEl);
-  };
+};
 
 var displayIssues = function (issues) {
 
@@ -77,4 +96,5 @@ var displayIssues = function (issues) {
     }
 };
 
-getRepoIssues("facebook/react");
+// getRepoIssues("facebook/react");
+getRepoName();
