254a3278484a2bec1102e922a76e499748efdc06
Sandy McCabe <sandytech13@gmail.com>
2022-03-12 20:40:36 -0600

    added listing of individual issues and pull requests for each repository

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -18,15 +18,21 @@ var getUserRepos = function (user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        if (response.ok) {
-          response.json().then(function(data) {
-            displayRepos(data, user);
-          });
-        } else {
-          alert("Error: GitHub User Not Found");
-        }
-      });
+    fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data, user);
+        });
+      } else {
+        alert('Error: GitHub User Not Found');
+      }
+    })
+    .catch(function(error) {
+      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+      alert("Unable to connect to GitHub");
+    });
 };
 
 var formSubmitHandler = function (event) {
@@ -46,6 +52,12 @@ var formSubmitHandler = function (event) {
 };
 
 var displayRepos = function(repos, searchTerm) {
+
+// check if api returned any repos
+if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
 
 // clear old content
 repoContainerEl.textContent = "";
